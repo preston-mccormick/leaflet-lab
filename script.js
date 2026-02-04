5a11794d0feb23e6772306d1ba4af34c985971bf
Preston McCormick <Preston.McCormick@aiworldwide.com>
2026-02-04 15:14:00 -0700

    Change default map style, starting locaiton, and initial zoom level

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const map = L.map("map").setView([39.8283, -98.5795], 4);
+const map = L.map("map").setView([40.0, -105.0], 15);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
+  maxZoom: 10,
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
