91739b820eacf087a43a4fad0ba0f78e4ed09051
Preston McCormick <Preston.McCormick@aiworldwide.com>
2026-02-05 15:50:07 -0700

    Move source files into src folder. Add title, tweak layout.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-const map = L.map("map").setView([40.0, -105.0], 15);
+const map = L.map("map").setView([40.0, -105.0], 10);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 10,
+  maxZoom: 15,
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
