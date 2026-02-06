2122a1e5e9843b1809d0772a5e263ef1952f2d78
Preston McCormick <Preston.McCormick@aiworldwide.com>
2026-02-05 17:16:03 -0700

    Start zoomed out further, but allow to zoom in further.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-const map = L.map("map").setView([40.0, -105.0], 10);
+const map = L.map("map").setView([40.0, -105.0], 8);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 15,
+  maxZoom: 21,
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
